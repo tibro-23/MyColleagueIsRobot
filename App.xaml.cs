e24ff98ee7ac4200c2a7d44ee68bbb4d9ba282c7
DESKTOP TIBRO <DESKTOP TIBRO@DESKTOP-JL890CK>
2022-10-20 22:44:25 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyColleagueIsRobot
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
